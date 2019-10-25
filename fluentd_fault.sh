a2921b6140a582a783be6ce89770ec7fcfccb4cc
Vladimir Pereskokov <38434572+gialloguitar@users.noreply.github.com>
2019-10-25 18:19:00 +0300

    vp_2

--- fluentd_fault.sh
+++ fluentd_fault.sh
@@ -16,7 +16,7 @@ WORKDIR=$(pwd)
 TS=$(date +'%H:%M:%S-%d:%m:%Y')
 LOG="$WORKDIR/fluentd_fault.log"    # Output LOG
 OS_USER="Vladimir_Pereskokov@epam.com"
-OS_PASS="100indeec$"
+OS_PASS="******"
 ANSIBLE_DIR="/opt/eco/ansible/inventory-paas"
 cd $ANSIBLE_DIR     # needed for custom ansible.cfg
 USAGE="\n
