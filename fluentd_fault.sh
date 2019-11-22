677bd1f5b937ffd68e662aaf55253a0ce53e014d
Vladimir Pereskokov <vladimir_pereskokov@epam.com>
2019-11-22 15:54:00 +0300

    fluentd updated

--- fluentd_fault.sh
+++ fluentd_fault.sh
@@ -11,12 +11,15 @@
 
 
 #set -x
+trap 'echo "Ctrl-C interrupted bu user..."; exit 1' SIGINT
 
-WORKDIR="/opt/eco/paas-39/fluentd"
+NOW=$(date +%Y-%m-%d)
 TS=$(date +'%H:%M:%S-%d:%m:%Y')
+FAULT_COUNT=32
+WORKDIR="/opt/eco/scripts/fluentd"
 USER="vladimir_pereskokov"
 OS_USER="$USER@epam.com"
-OS_PASS="*****"
+OS_PASS="****"
 ANSIBLE_DIR="/opt/eco/ansible/inventory-paas"
 USAGE="\n
 Wrong options \n
@@ -30,10 +33,10 @@ SCRIPTNAME <dev|prod> check - that command use for check count of output buffers
 cd $ANSIBLE_DIR     # needed for custom ansible.cfg
 
 case "$1" in
-dev )
+new )
 # 3.11 Dev
-ANSIBLE_INVENTORY="$ANSIBLE_DIR/inventories/hosts-paas-311-develop"
-OS_HOST="https://web.paas.lab.epam.com:8443"
+ANSIBLE_INVENTORY="$ANSIBLE_DIR/inventories/hosts-paas-311"
+OS_HOST="https://web.paas.epam.com:8443"
 LOGGING_PROJ="openshift-logging"
 ;;
 prod )
@@ -48,12 +51,19 @@ exit 0
 ;;
 esac
 
+ANSIBLE_CALL="ansible nodes -u $USER -i $ANSIBLE_INVENTORY -o -m shell -a "
+
 case "$2" in
 check )
 while true; do
 echo "Check $1 environment..."
-ansible nodes -u $USER -i $ANSIBLE_INVENTORY -o -m shell -a "ls /var/lib/fluentd | wc -l "| awk '{if($8 > 32) { print "Node " tolower($1 " has " $8 " output buffers")}}'
+
+echo ''
+$ANSIBLE_CALL"ls -l --time-style=full /var/lib/fluentd/*.log|tail -n1"|awk -v n=$NOW '{if ($13 !~ n && $13 !~ ".*[a-zA-Zа-яА-Я].*|^$") {print "Node " tolower($1 " has too old buffers since " $13)}}'
+echo ''
+$ANSIBLE_CALL"ls /var/lib/fluentd | wc -l "| awk -v f=$FAULT_COUNT '{if($8 > f && $8 !~ ".*[a-zA-Zа-яА-Я].*|^$") { print "Node " tolower($1 " has " $8 " output buffers")}}'
 echo "---"
+
 sleep 10
 done
 exit 0
@@ -82,8 +92,12 @@ os_context=$(oc whoami --show-context)
 echo "$TS - $os_context"
 
 # if flunentd output buffers count will reach to 33, it pod will considered is hung
-fault_nodes=( $(ansible nodes -u $USER -i $ANSIBLE_INVENTORY -o -m shell -a "ls /var/lib/fluentd | wc -l "| \
-awk '{if($8 > 32) { print tolower($1)}}') )
+fault_nodes=( $($ANSIBLE_CALL -o -m shell -a "ls /var/lib/fluentd | wc -l "| \
+awk -v f=$FAULT_COUNT '{if($8 > f && $8 !~ ".*[a-zA-Zа-яА-Я].*|^$") { print tolower($1)}}';\
+$ANSIBLE_CALL"ls -l --time-style=full /var/lib/fluentd/*.log|tail -n1"| \
+awk -v n=$NOW '{if ($13 !~ n && $13 !~ ".*[a-zA-Zа-яА-Я].*|^$") {print tolower($1)}}'
+) )
+
 count_of_fault_pods=${#fault_nodes[@]}
 
 if [[ $count_of_fault_pods > 0 ]]; then
@@ -105,4 +119,3 @@ fi
 oc logout
 
 exit 0
-
