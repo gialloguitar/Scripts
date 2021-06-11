9c6cbb7396270cc9c308f1e7a437621d2b84ccb6
Vladimir Pereskokov <markule@wisebits.com>
2021-06-11 03:16:53 +0300

    update sites backup

--- backup_sites.sh
+++ backup_sites.sh
@@ -1,72 +1,60 @@
 #! /bin/bash
-# Срипт работает в кроне, базы и саты для бэкапа перечислены в массиве DBASES и SITES соответственно
-# так же срипт можно запускать вручную, через пробел указав необходимые базы (каталоги бэкапятся только в кроне).
-# Бэкапы  сжаты Гзипом и лежат на сервере.
-#
 
-# current date
-DATE=`date +%F`
-# y/m/d/h/m separately
-YEAR=`date +%Y`
-MONTH=`date +%m`
-DAY=`date +%d`
-HOURS=`date +%H`
-MINUTES=`date +%M`
-#DB credentials
-DBUSER="*****"
-DBPASS="*****"
-DBHOST="localhost"
-#databases and sites for backup
-DBASES=('social_st' 'social_pr')  #
-SITES=('social')
-#mount path
-MPATH="/media/"
+set -e
 
-mysql -h $DBHOST -u $DBUSER -p$DBPASS -e "show databases;" > /opt/bases_tmp
-sed 1,1d /opt/bases_tmp > /opt/bases
-rm /opt/bases_tmp
-#BASELIST=`cat /opt/bases`
+DATE=$(date +%F)
+SITES_DIR="/home/www/sites"
+BACKUP_DIR="/opt/aws/backup/sites/$DATE"
 
-#Paths
+DATABASES=('db1' 'db2')
+SITES=('site1' 'site2')
 
-#/sbin/mount.cifs //192.168.000.000/d$/backup/ubuntu "$MPATH" -o user=super,password=$DBPASS,domain=DOM
-mount -a       # Все пути лучше из fstab монтировать
-BACKUP_DIR="$MPATH/BackupSites/$YEAR-$MONTH-$DAY/Time-$HOURS-$MINUTES"
-mkdir --parents $BACKUP_DIR
-cd $BACKUP_DIR
+mount -a
 
-if [[ $# > 0 ]];then
-echo "Бэкап $# баз данных вручную:"
-             for i in $@;do
-                      if grep -w $i /opt/bases;then
-backup_name="$YEAR-$MONTH-$DAY.$HOURS-$MINUTES.$i.backup.sql"
-backup_tarball_name="$backup_name.tar.gz"
-echo "Dump of $i"
-mysqldump -h "$DBHOST" --databases "$i" -u "$DBUSER" --password="$DBPASS" > "$backup_name"
-echo "Compress..."
-tar -zcf "$backup_tarball_name" "$backup_name"
-rm "$backup_name"
-echo "Done."
-echo
-                     else
-                        echo "База данных $i не обнаружена, проверьте написание!"
-                        echo
-                     fi
-            done
-      elif [[ $# == 0 ]];then
-            for database in ${DBASES[@]};do
-backup_name="$YEAR-$MONTH-$DAY.$HOURS-$MINUTES.$database.backup.sql"
-backup_tarball_name="$backup_name.tar.gz"
-mysqldump -h "$DBHOST" --databases "$database" -u "$DBUSER" --password="$DBPASS" > "$backup_name"
-tar -zcf "$backup_tarball_name" "$backup_name"
-rm "$backup_name"
-            done
-#####Архивация каталогов сайтов
-mkdir sites
-cd sites
-       for j in ${SITES[@]};do
-tar -zcf "$YEAR-$MONTH-$DAY.$HOURS-$MINUTES.$j.tar.gz" "/home/$j"
-       done
+_backup_database() {
+    cd /tmp
+    mkdir -p $BACKUP_DIR
+    local backup_name="$1.backup.sql"
+    local archive_name="${BACKUP_DIR}/${backup_name}.tar.gz"
+
+    echo -e "\nBackup database '$1':\n"
+    mysqldump --databases $1  > $backup_name
+    tar -czvf $archive_name $backup_name
+    rm -f $backup_name
+}
+
+# Databases backup
+if [[ $# > 0 ]]
+then
+  for db in $@
+  do
+    if mysql -e "show databases;" | grep -w $db
+    then
+      _backup_database $db
+    else
+      echo -e "\nDatabase '$db' is not exist.\n"
+    fi
+  done
+elif [[ $# == 0 ]]
+then
+  for db in ${DATABASES[@]}
+  do
+    _backup_database $db
+  done
+
+# Sites files backup
+mkdir -p $BACKUP_DIR
+cd $SITES_DIR
+for site in ${SITES[@]}
+do
+  backup_name="$site.tar.gz"
+  archive_name="${BACKUP_DIR}/${site}.tar.gz"
+
+  echo -e "\nBackup site files '$site':\n"
+  tar -czf $archive_name "${SITES_DIR}/${site}"
+
+done
 fi
-#cd ~
-umount -l "$MPATH"
+
+echo -e "\nFinish."
+
